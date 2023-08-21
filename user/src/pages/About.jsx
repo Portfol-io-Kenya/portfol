a6cc5422d2bcd18ae8e8a168214ab0bc26d5cdf7
moses <cayvoh@mosess-MacBook-Pro.local>
2023-08-21 11:23:24 +0300

    new corrections

--- user/src/pages/About.jsx
+++ user/src/pages/About.jsx
@@ -8,39 +8,21 @@ const About = () => {
         <div className='w-full md:2/3 2xl:w-2/4'>
           <h1 className='text-3xl text-blue-600 font-bold mb-5'>About Us</h1>
           <p className='text-justify leading-7'>
-            Microsoft Corporation and its contributors are available at
-            http://www.microsoft.com and at http://www.microsoft.com for more
-            information about the contributors and contributors to the Microsoft
-            Corporation and its contributors to the Microsoft Corporation and
-            its contributors to the Microsoft Corporation
+          Portfolio is a start-up freelance job marketplace that aims to connect skilled
+          African freelancers with clients from all over the world. Our platform offers a
+          secure and user-friendly environment for both freelancers and clients, providing
+          access to quality services and job opportunities. Our business model is based on
+          taking a commission from each successful transaction between clients and freelancers. 
+          Our goal is to become the go-to freelance job marketplace in Africa and expand our
+          services to other regions.
+          The freelance job marketplace in Africa is rapidly growing, with a significant demand
+          for quality services from clients worldwide. However, many skilled African freelancers
+          often lack access to job opportunities and are unable to showcase their skills to potential
+          clients. Our platform aims to bridge this gap by offering a secure and reliable environment
+          for freelancers to showcase their skills and connect with potential clients.
           </p>
         </div>
         <img src={JobImg} alt='About' className='w-auto h-[300px]' />
-      </div>
-
-      <div className='leading-8 px-5 text-justify'>
-        <p>
-          Microsoft Corporation and its contributors are available at
-          http://www.microsoft.com and at http://www.microsoft.com for more
-          information about the contributors and contributors to the Microsoft
-          Corporation and its contributors to the Microsoft Corporation and its
-          contributors to the Microsoft Corporation Microsoft Corporation and
-          its contributors are available at http://www.microsoft.com and at
-          http://www.microsoft.com for more information about the contributors
-          and contributors to the Microsoft Corporation and its contributors to
-          the Microsoft Corporation and its contributors to the Microsoft
-          Corporation Microsoft Corporation and its contributors are available
-          at http://www.microsoft.com and at http://www.microsoft.com for more
-          information about the contributors and contributors to the Microsoft
-          Corporation and its contributors to the Microsoft Corporation and its
-          contributors to the Microsoft Corporation Microsoft Corporation and
-          its contributors are available at http://www.microsoft.com and at
-          http://www.microsoft.com for more information about the contributors
-          and contributors to the Microsoft Corporation and its contributors to
-          the Microsoft Corporation and its contributors to the Microsoft
-          Corporation Microsoft Corporation and its contributors are available
-          at http://www.microsoft.com and at http://www.microsoft.com for more
-        </p>
       </div>
     </div>
   );
--- user/src/pages/UploadJob.jsx
+++ user/src/pages/UploadJob.jsx
@@ -162,7 +162,7 @@ const UploadJob = () => {
             <TextInput
               name='location'
               label='Job Location'
-              placeholder='eg. New York'
+              placeholder='eg. Nairobi'
               type='text'
               register={register("location", {
                 required: "Job Location is required",
@@ -211,7 +211,7 @@ const UploadJob = () => {
                 <CustomButton
                   type='submit'
                   containerStyles='inline-flex justify-center rounded-md border border-transparent bg-blue-600 px-8 py-2 text-sm font-medium text-white hover:bg-[#1d4fd846] hover:text-[#1d4fd8] focus:outline-none '
-                  title='Sumbit'
+                  title='Submit'
                 />
               )}
             </div>
